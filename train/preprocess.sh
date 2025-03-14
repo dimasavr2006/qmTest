b2e508553f6c6d649526aeaeaadd86a55ab10fc4
Dmitriy <163722491+dimasavr2006@users.noreply.github.com>
2025-03-14 16:10:25 +0300

    Update preprocess.sh

--- train/preprocess.sh
+++ train/preprocess.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Dataset.
-dataset=QM9under7atoms_atomizationenergy_eV
+# dataset=QM9under7atoms_atomizationenergy_eV
 # dataset=QM9under14atoms_atomizationenergy_eV
 # dataset=QM9full_atomizationenergy_eV
-# dataset=QM9under7atoms_homolumo_eV  # Two properties (homo and lumo).
+dataset=QM9under7atoms_homolumo_eV  # Two properties (homo and lumo).
 # dataset=QM9full_homolumo_eV
 # dataset=yourdataset_property_unit
 
