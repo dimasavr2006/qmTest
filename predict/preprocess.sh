eb138b17fa9c51784ff905ede10784d624754fbc
Dmitriy <163722491+dimasavr2006@users.noreply.github.com>
2025-03-18 12:40:50 +0300

    Update preprocess.sh

--- predict/preprocess.sh
+++ predict/preprocess.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Dataset used in pre-training.
-dataset_trained=QM9under14atoms_atomizationenergy_eV
+# dataset_trained=QM9under14atoms_atomizationenergy_eV
 # dataset_trained=QM9full_atomizationenergy_eV
 # dataset_trained=QM9full_homolumo_eV  # Two properties (homo and lumo).
 # dataset_trained=yourdataset_property_unit
+dataset=QM9under7atoms_homolumo_eV
 
 # Basis set and grid field used in pre-training.
 basis_set=6-31G
@@ -12,6 +13,6 @@ radius=0.75
 grid_interval=0.3
 
 # Dataset for prediction.
-dataset_predict=QM9over15atoms_atomizationenergy_eV  # Extrapolation.
+dataset_predict=yourdataset_property_unit  # Extrapolation.
 
 python preprocess.py $dataset_trained $basis_set $radius $grid_interval $dataset_predict
